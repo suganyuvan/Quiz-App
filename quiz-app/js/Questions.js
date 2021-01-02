a7575eac542c3ff32f39889e94b5125a34c7dd44
sugavaneshwaran <suganyyvi77@gmail.com>
2021-01-02 20:12:37 +0530

    first commit

---
+++ quiz-app/js/Questions.js
@@ -0,0 +1,58 @@
+//creating an array and passing the number, questions, options and answers
+let questions = [
+    {
+        numb:1,
+        question: 'What does HTML stands for?',
+        answers:'Hyper Text Markup Langauge',
+        options:[
+            'Hyper Text Preprocessor',
+            'Hyper Text Markup Langauge',
+            'Hyper Text Multiple Langauge',
+            'Hyper Tool Multi Language'
+        ]
+    },
+    {
+        numb:2,
+        question: 'What does CSS stands for?',
+        answers:'Cascading Style Sheet',
+        options:[
+            'Common Style Sheet',
+            'Colorful Style Sheet',
+            'Computer Style Sheet',
+            'Cascading Style Sheet'
+        ]
+    },
+    {
+        numb:3,
+        question: 'What does PHP stands for?',
+        answers:'Hypertext Preprocessor',
+        options:[
+            'Hypertext Preprocessor',
+            'Hypertext Programming',
+            'Hypertext Preprogramming',
+            'Hometext Preprocessor'
+        ]
+    },
+    {
+        numb:4,
+        question: 'What does SQL stands for?',
+        answers:'Structured Query Langauge',
+        options:[
+            'Stylish Question Langauge',
+            'Stylesheet Query Langauge',
+            'Statement Question Langauge',
+            'Structured Query Langauge'
+        ]
+    },
+    {
+        numb:5,
+        question: 'What does XML stands for?',
+        answers:'eXtensible Markup Langauge',
+        options:[
+            'eXtensible Markup Langauge',
+            'eXecutable Multiple Langauge',
+            'eXTra Multi-Program Langauge',
+            'eXamine Multiple Langauge'
+        ]
+    }
+]
